763122d347fd260f0f871756e0898422b29ca4ef
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-03-31 16:22:57 +0530

    database created

---
+++ src/configs/db.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose');
+
+module.exports= ()=>{
+    return mongoose.connect("mongodb+srv://bookmyshow:bookmyshow@bookmyshow.kteht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+}
